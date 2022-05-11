9b047486d91330185dd8a855e22b32c11905fd67
Virslandir <38184251+Virslandir@users.noreply.github.com>
2022-05-11 20:08:23 +0200

    Add project files.

---
+++ PlayerLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerLibrary.Models
+{
+    public class GridSpotModel
+    {
+        public string SpotLetter { get; set; }
+        public string SpotNumber { get; set; }
+        public GridSpotStatus Status { get; set; } = GridSpotStatus.Empty;
+    }
+}
---
+++ PlayerLibrary/Models/PlayerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerLibrary.Models
+{
+    public class PlayerModel
+    {
+        public string PlayerName { get; set; }
+        public List<string> ShipLocations { get; set; }
+        //public List<GridSpotModel> ShipLocations { get; set; }
+        //public List<GridSpotModel> ShotGrid { get; set; }
+        public int ShipCount = 5;
+        public List<string> AttemptedSpots { get; set; }
+
+        public PlayerModel ()
+        {
+            AttemptedSpots = new List<string>();
+            ShipLocations = new List<string>();
+        }
+    }
+}
